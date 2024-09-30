5efa479e5665ba93028ae5d4f8bebd334cf5fd8e
williamlab123 <williamprv@outlook.com>
2024-09-29 21:57:20 -0300

    .

--- main.cpp
+++ main.cpp
@@ -3,6 +3,8 @@
 #include "Player.h"
 #include "LifeController.h"
 #include "Map.h"
+#include "Battle.h"
+#include "Enemy.h"
 
 using namespace std;
 
@@ -29,8 +31,10 @@ int main()
     cout << map.size << endl;
     Start();
     Rules();
+    CreateEnemies();
+    Battle battle = Battle(Player("Player", LifeController()), Enemy("Goblin", LifeController()));
     
- 
+
     return 0;
 }
 
@@ -41,8 +45,47 @@ void Start()
     cout << "You must escape the dungeon" << endl;
 }
 
+void CreateEnemies()
+{
+    Enemy goblin = Enemy("Goblin", LifeController());
+    goblin.lifeController.maxLife = 100;
+
+    Enemy orc = Enemy("Orc", LifeController());
+    orc.lifeController.maxLife = 150;
+
+    Enemy troll = Enemy("Troll", LifeController());
+    troll.lifeController.maxLife = 200;
+
+    Enemy dragon = Enemy("Dragon", LifeController());
+    dragon.lifeController.maxLife = 300;
+
+    Enemy ghost = Enemy("Ghost", LifeController());
+    ghost.lifeController.maxLife = 350;
+
+    Enemy skeleton = Enemy("Skeleton", LifeController());
+    skeleton.lifeController.maxLife = 375;
+
+    Enemy zombie = Enemy("Zombie", LifeController());
+    zombie.lifeController.maxLife = 450;
+
+    Enemy vampire = Enemy("Vampire", LifeController());
+    vampire.lifeController.maxLife = 500;
+
+    Enemy werewolf = Enemy("Werewolf", LifeController());
+    werewolf.lifeController.maxLife = 550;
+
+    Enemy Felipe = Enemy("Felipe", LifeController());
+    Felipe.lifeController.maxLife = 1000000;
+}
+
 void Rules()
 {
     cout << "The game has 10 sqms, to finish the game you must reach the 10th sqm" << endl;
     cout << "Each sqm can have a monster or an item" << endl;
+}
+
+void End()
+{
+    cout << "You have reached the end of the dungeon" << endl;
+    cout << "You have escaped the Felps dungeon" << endl;
 }
